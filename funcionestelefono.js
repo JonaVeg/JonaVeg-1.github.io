02d6aaf2ec85f4cf336f10d2332b61d8f82c518a
Jonathan <utp0155232@alumno.utpuebla.edu.mx>
2023-11-29 19:09:19 -0600

    Funciones.JVG

---
+++ funcionestelefono.js
@@ -0,0 +1,8 @@
+function realizarLlamada() {
+    // Reemplaza el número de teléfono con el que deseas que se realice la llamada
+    var numeroTelefono = '2225402887';
+    
+    // Utiliza la API de marcación tel: para iniciar la llamada
+    window.location.href = 'tel:' + numeroTelefono;
+  }
+  
