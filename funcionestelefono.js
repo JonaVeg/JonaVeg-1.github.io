64a22b7ce3cfee72e33d2b76734e82dfb7f9e813
Jonathan <utp0155232@alumno.utpuebla.edu.mx>
2024-02-16 16:59:47 -0600

    Funcionesportafolio.JVG

--- funcionestelefono.js
+++ funcionestelefono.js
@@ -2,9 +2,11 @@ function realizarLlamada() {
     // Reemplaza el número de teléfono con el que deseas que se realice la llamada
     var numeroTelefono = '2225402887';
 
-    // Construye la URL de WhatsApp con el número de teléfono.
-    var urlWhatsApp = 'https://wa.me/' + numeroTelefono;
-    // Redirige a la URL de WhatsApp.
+    // Reemplaza 'Hola, estoy interesado en cotizar. ¿Cómo puedo empezar?' con tu mensaje predeterminado.
+    var mensaje = encodeURIComponent('Hola, estoy interesado en cotizar. ¿Cómo puedo empezar?');
+
+    // Construye la URL de WhatsApp con el número de teléfono y el mensaje predeterminado.
+    var urlWhatsApp = 'https://wa.me/' + numeroTelefono + '?text=' + mensaje;
     window.location.href = urlWhatsApp;
     
     // Utiliza la API de marcación tel: para iniciar la llamada
